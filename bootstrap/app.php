2f4dbec5fe35f4ddd81d7d30dc9edb5fd2975b62
abdulrofiq-26 <rofiqabdul983@gmail.com>
2026-08-18 18:51:15 +0700

    feat: initialize Laravel application bootstrap and configure Vercel environment variables

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,11 +1,9 @@
 <?php
-
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-// 1. Buat dan simpan aplikasinya ke dalam variabel $app terlebih dahulu
-$app = Application::configure(basePath: dirname(__DIR__))
+return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -17,11 +15,3 @@ $app = Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
-
-// 2. Ubah path storage HANYA jika sedang berjalan di Vercel
-if (isset($_ENV['VERCEL'])) {
-    $app->useStoragePath('/tmp/storage');
-}
-
-// 3. Kembalikan aplikasi yang sudah dimodifikasi
-return $app;
